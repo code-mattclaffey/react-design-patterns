e85309ffb077d86d9c2496e3e17a315f7881489d
code-mattclaffey <mattclaffey93@gmail.com>
2024-06-25 16:23:20 +0100

    Update code block to shades of purple

--- .storybook/components/CodeBlock.tsx
+++ .storybook/components/CodeBlock.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { CodeBlock, dracula } from 'react-code-blocks';
+import { CodeBlock, shadesOfPurple } from 'react-code-blocks';
 
 export const Code = ({ children }) => (
   <CodeBlock
     text={children}
     language="jsx"
     showLineNumbers={true}
-    theme={dracula}
+    theme={shadesOfPurple}
   />
 );
