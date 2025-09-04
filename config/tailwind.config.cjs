36a3ad29ea493b698c5a01dab27966f39e1ded35
code-mattclaffey <mattclaffey93@gmail.com>
2025-09-04 21:07:03 +0100

    chore: tidy up config

---
+++ config/tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
+  darkMode: ['class', '[data-mode="dark"]'],
+  theme: {
+    extend: {
+      colors: {
+        modal: {
+          bg: 'rgba(0, 0, 0, 0.3)'
+        },
+        code: {
+          950: '#1E1E3F',
+          750: '#2D2B55',
+          600: '#a03fc0',
+          500: '#A599E9'
+        }
+      }
+    }
+  },
+  plugins: []
+};
